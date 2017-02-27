a447cf7e11f9b1ea9e3ef2349aa04c658084ff87
DanielCauser <danielcauser@gmail.com>
2017-02-26 23:19:24 -0300

    Changes to sync with azure

--- LAB-MVVM/LAB_MVVM/Models/Repository.cs
+++ LAB-MVVM/LAB_MVVM/Models/Repository.cs
@@ -9,17 +9,24 @@ namespace LAB_MVVM.Models
     public class Repository
     {
 
+        //public async Task<List<Cat>> GetCats()
+        //{
+        //    List<Cat> Cats;
+        //    var URLAPI = "http://demos.ticapacitacion.com/cats";
+
+        //    using (var client = new System.Net.Http.HttpClient())
+        //    {
+        //        var JSON = await client.GetStringAsync(URLAPI);
+        //        Cats = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Cat>>(JSON);
+        //    }
+        //    return Cats;
+        //}
+
         public async Task<List<Cat>> GetCats()
         {
-            List<Cat> Cats;
-            var URLAPI = "http://demos.ticapacitacion.com/cats";
-
-            using (var client = new System.Net.Http.HttpClient())
-            {
-                var JSON = await client.GetStringAsync(URLAPI);
-                Cats = Newtonsoft.Json.JsonConvert.DeserializeObject<List<Cat>>(JSON);
-            }
-            return Cats;
+            var Service = new Services.AzureService<Cat>();
+            var Items = await Service.GetTable();
+            return Items.ToList();
         }
 
     }
